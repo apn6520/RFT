bf62c0dfedc9da1e27605182c3c213389d02bc9a
apn6250 <amar.patil1@crest.in>
2016-10-24 11:54:37 +0530

    removed commented code - success

--- src/TransferDesk.MS.Web/Controllers/ReviewerIndexController.cs
+++ src/TransferDesk.MS.Web/Controllers/ReviewerIndexController.cs
@@ -30,7 +30,7 @@ namespace TransferDesk.MS.Web.Controllers
         {
             try
             {
-                //string conString = string.Empty;
+                string conString = string.Empty;
                 conString = Convert.ToString(ConfigurationManager.AppSettings["dbTransferDeskService"]);
                 _reviewerIndexDBRepositoriesReadSite = new ReviewerIndexDBRepositoriesReadSite(conString, Logger);
                 _reviewerIndexDBContext = new ReviewerIndexDBContext(conString);
